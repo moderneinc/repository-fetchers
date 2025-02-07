2509f01254329ba8b7c950a0ea595ce472878821
Bryce Tompkins <167870666+bryceatmoderne@users.noreply.github.com>
2025-02-07 16:12:32 -0500

    Paginate results in `bitbucket-data-center.sh` (#6)
    * Paginate results in bitbucket-data-center.sh
    
    * Support ssh clone URLs

--- bitbucket-data-center.sh
+++ bitbucket-data-center.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ -z "$1" ]; then
     echo "Usage: $0 <bitbucket_url>"
     echo "Example: $0 https://my-bitbucket.com/stash"
@@ -10,23 +9,41 @@ fi
 bitbucket_url=$1
 auth_header=""
 
-if [ -n "$AUTH_TOKEN" ]; then
-    auth_header="Authorization: Bearer $AUTH_TOKEN"
-fi
-
-ALL_REPOS=$(curl -s -X GET -H "Content-Type: application/json" -H "$auth_header" "$bitbucket_url/rest/api/1.0/repos"| jq -r '.values[] | [.slug, .project.key, (.links.clone[] | select(.name == "http").href)] | @csv')
-if [ $? -ne 0 ]; then
-    echo "Error occurred while retrieving repository list."
+if [ -z "$AUTH_TOKEN" ]; then
+    echo "Please set the AUTH_TOKEN environment variable."
     exit 1
 fi
 
-echo "cloneUrl,branch,org"
-for REPO in $ALL_REPOS; do
-    IFS=',' read -r repo project cloneUrl <<< "$REPO"
-    repo="${repo//\"/}"
-    project="${project//\"/}"
-    cloneUrl="${cloneUrl//\"/}"
-    branch=$(curl -s -X GET -H "Content-Type: application/json" -H "$auth_header" "$bitbucket_url/rest/api/latest/projects/$project/repos/$repo/default-branch" | jq -r '.displayId')
+if [ -z "$CLONE_PROTOCOL" -o "$CLONE_PROTOCOL" != "ssh" ]; then
+    CLONE_PROTOCOL=http
+fi
 
-    echo "$cloneUrl,$branch,$project"
+nextPage=1
+echo '"cloneUrl","branch","org"'
+while :; do
+    # Construct the request URL with pagination parameters
+    request_url="$bitbucket_url/rest/api/1.0/repos?start=$nextPage&limit=100"
+
+    # Fetch the data
+    response=$(curl --silent -H "Content-Type: application/json" -H "Authorization: Bearer $AUTH_TOKEN" "$request_url")
+
+    if [ $? -ne 0 ]; then
+        echo "Error occurred while retrieving repository list."
+        exit 1
+    fi
+
+    # Process and output data
+    if [ "$CLONE_PROTOCOL" = "http" ]; then
+        echo "$response" | jq -r '.values[] | [(.links.clone[] | select(.name == "http").href), .slug, .project.key] | @csv'
+    else 
+        echo "$response" | jq -r '.values[] | [(.links.clone[] | select(.name == "ssh").href), .slug, .project.key] | @csv'
+    fi
+
+
+    isLastPage=$(echo "$response" | jq '. | .isLastPage') 
+    if [ "$isLastPage" = "true" ]; then
+        exit
+    fi
+
+    nextPage=$(echo "$response" | jq '. | .nextPageStart') 
 done
