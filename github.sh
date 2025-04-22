c494842cf227cd7eae279a2e7528070cc4a78629
Greg Oledzki <greg.oledzki@moderne.io>
2025-04-22 16:48:49 +0200

    Filter out archived repos (#10)
    * Filter out archived repos
    
    * Remove extra jq call
    
    * Add ssh:// URL version as a comment
    
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    
    ---------
    
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>

--- github.sh
+++ github.sh
@@ -8,5 +8,6 @@ fi
 
 organization=$1
 
-# JQ
-gh api --paginate "orgs/$organization/repos" | jq -r 'first(["cloneUrl","branch","org"]), (.[] | [.clone_url, .default_branch, "'"$organization"'"]) | @csv'
+echo "\"cloneUrl\",\"branch\",\"org\""
+gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) | [.clone_url, .default_branch, "'"$organization"'"] | @csv' | sort
+#gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) | [.ssh_url, .default_branch, "'"$organization"'"] | @csv' | sort
