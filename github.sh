5607cb289a54b672971cfaf7d528b0ed51717a51
Kevin Carpenter™️ <kevin@moderne.io>
2025-09-22 18:51:09 -0300

    Add origin and path for GitHub (#17)

--- github.sh
+++ github.sh
@@ -8,6 +8,14 @@ fi
 
 organization=$1
 
-echo "\"cloneUrl\",\"branch\",\"org\""
-gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) | [.clone_url, .default_branch, "'"$organization"'"] | @csv' | sort
-#gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) | [.ssh_url, .default_branch, "'"$organization"'"] | @csv' | sort
+echo "\"cloneUrl\",\"branch\",\"origin\",\"path\",\"org\""
+gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) |
+  .clone_url as $url |
+  ($url | sub("https://"; "") | sub("/"; " ") | split(" ")[0]) as $origin |
+  ($url | sub("https://[^/]+/"; "") | sub("\\.git$"; "")) as $path |
+  [$url, .default_branch, $origin, $path, "'"$organization"'"] | @csv' | sort
+#gh api --paginate "orgs/$organization/repos" --jq '.[] | select(.archived == false) |
+#  .ssh_url as $url |
+#  ($url | sub("git@"; "") | sub(":"; " ") | split(" ")[0]) as $origin |
+#  ($url | sub("git@[^:]+:"; "") | sub("\\.git$"; "")) as $path |
+#  [$url, .default_branch, $origin, $path, "'"$organization"'"] | @csv' | sort
